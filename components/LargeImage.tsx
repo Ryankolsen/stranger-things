521b623415e9ca4446f9043994d82e181ac256e0
Ryan Olsen <ryankolsen@gmail.com>
2022-10-23 19:07:42 -0400

    style rounded images and hover on close btn

--- components/LargeImage.tsx
+++ components/LargeImage.tsx
@@ -13,13 +13,14 @@ function LargeImage(props: Props) {
   }
   return (
     <>
-      <div className="flex justify-center">
+      <div className="flex justify-center ">
         <div>
           <button
             onClick={handleCloseClick}
             className="w-full absolute -mt-5 text-end pr-2 justify-end  z-20 "
           >
             <Image
+              className=" hover:scale-125"
               src={MobileClosedDark}
               alt="Close image window"
               height={20}
@@ -29,7 +30,7 @@ function LargeImage(props: Props) {
         </div>
         <div className=" flex h-auto  p-1 sm:p-4"></div>
         <Image
-          className="object-cover object-top"
+          className="object-cover object-top rounded"
           src={imageSource}
           alt="image of mars"
           width={700}
